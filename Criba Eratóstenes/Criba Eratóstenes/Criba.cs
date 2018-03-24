7f3796803c81c3625fd90e048d7670e0ed0b9cd6
Josz27 <36236045+Josz27@users.noreply.github.com>
2018-03-23 21:39:19 -0600

    Criba eratóstenes
    Números primos menores a 1000 con la Criba de eratóstenes

---
+++ Criba Eratóstenes/Criba Eratóstenes/Criba.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criba_Eratóstenes
+{
+    class Criba
+    {
+        private static bool[] _primos;//Vector de booleans
+
+       
+
+        public static void proceso(int x) //Se toma el primer número no tachado(Notas mías, es de una página, lo pegué aquí para apoyarme) y se eleva al cuadrado, si el resultado es menor que max se tachan todos sus múltiplos
+
+        {
+            _primos = new bool[x + 1];//Llenar hasta el límite
+            for (int z = 0; z < x + 1; z++)
+            {
+                _primos[z] = true;
+            }
+         
+            for (int inicio = 2; inicio < Math.Sqrt(_primos.Length); inicio++) // Se nos dice que iniciemos desde el 2.
+            {
+                if (_primos[inicio]==true)
+                {
+                    for (int e = inicio+1; e < _primos.Length; e++)  //Tomamos el siguiente número que no fue tachado.
+                       
+                    {
+                        if (e % inicio == 0)
+                        {
+                            _primos[e] = false; //Tachamos los múltiplos
+                        }
+
+                        }
+
+                     }
+
+                     }
+                     }
+        // Igual se podría hacer en un método todo pero apenas y le entendí a lo que hice =) ...Si veo esto después de vacaciones no le voy a entender ni madres.
+            
+            public static string obtener(int x)
+         {
+            proceso(x);// Uso del método anterior para no hacer un desmadre en uno solo.
+            string resultado = " ";
+            for (int inicio = 2; inicio < _primos.Length; inicio++) //Comenzamos desde 2 como dice la fórmula.
+            if (_primos[inicio]==true)
+
+            resultado =resultado+inicio + ",";
+
+            return resultado;
+         }
+            }
+      }
+// Tiene dudas de mis notas? piensa que lo copié de internet? Nel, Mire, https://aureliux.files.wordpress.com/2013/03/cribaeratostenes.pdf ni siquiera usaron vectores, solamente usé los textos que explicaban el procedimiento para hacerlo con vectores...
---
+++ Criba Eratóstenes/Criba Eratóstenes/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criba_Eratóstenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Los Números Primos son los siguientes: "+Environment.NewLine;
+            string[] primosfinales = Criba.obtener(1000).Split();
+
+
+            for (int i = 0; i < primosfinales.Length; i++)
+            {
+                textBox1.Text += primosfinales[i] + Environment.NewLine;
+            }
+              
+        }
+    }
+}
