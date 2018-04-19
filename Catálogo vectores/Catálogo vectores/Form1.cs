d5d521d603ff990b51bf08e6ba9321b4b8540e32
Josz27 <36236045+Josz27@users.noreply.github.com>
2018-04-18 21:39:00 -0500

    Catálgo vectores

---
+++ Catálogo vectores/Catálogo vectores/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catálogo_vectores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Catálogo c1 = new Catálogo();
+        int c;
+        
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = c1.buscarEspacio();
+            
+            
+               
+            
+            //if (c==-1)
+            //{
+            //    MessageBox.Show("No hay espacio para más productos");
+            //}
+           
+           
+            c1.agregarproducto(c, Convert.ToInt32(textBox1.Text), textBox2.Text, Convert.ToInt32(textBox3.Text), Convert.ToDouble(textBox4.Text));
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            
+        }
+        
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string s = " ";
+
+          s=c1.mostrarCatálogo();
+            textBox5.Text = s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            string s = " ";
+            s+= c1.Buscar(Convert.ToInt32(textBox1.Text));
+            textBox5.Text = s;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (c1.seEncontro(Convert.ToInt16(textBox1.Text)) == true)
+            {
+                c--;
+            }
+            c1.Eliminar(Convert.ToInt16(textBox1.Text));
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+           
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int c = Convert.ToInt32(textBox6.Text)-1;
+            c1.agregarproducto(c, Convert.ToInt32(textBox1.Text), textBox2.Text, Convert.ToInt32(textBox3.Text), Convert.ToDouble(textBox4.Text));
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+        }
+    }
+}
---
+++ Catálogo vectores/Catálogo vectores/Producto.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catálogo_vectores
+{
+    class Catálogo
+    {
+        int[] codigo = new int[15];
+        string[] nombre = new string[15];
+        int[] cantidad = new int[15];
+        double[] costo = new double[15];
+
+        public void agregarproducto(int posicion, int codigos, string nombres, int cantidades, double costos)
+        {
+            codigo[posicion] = codigos;
+            nombre[posicion] = nombres;
+            cantidad[posicion] = cantidades;
+            costo[posicion] = costos;
+
+        }
+        public string mostrarCatálogo()
+        {
+            string s = "";
+            int x = 1;
+            for (int c = 0; c < 15; c++)
+            {
+                s += "Datos Producto " + x + " Código: " + codigo[c].ToString() + " " + "Nombre: " + nombre[c] + " " + "Cantidad: " + cantidad[c].ToString() + " " + "Costo: " + costo[c].ToString() + Environment.NewLine;
+                x++;
+            }
+            return s;
+        }
+
+        public string Buscar(int codigoProducto)
+        {
+            string s = "";
+            for (int c = 0; c < 15; c++)
+            {
+                if (codigoProducto == codigo[c])
+                {
+                    s = s += "Datos Producto " + " Código: " + codigo[c].ToString() + " " + "Nombre: " + nombre[c] + " " + "Cantidad: " + cantidad[c].ToString() + " " + "Costo: " + costo[c].ToString();
+                    return s;
+
+                }
+                else
+                {
+                    s = "No se encontró el producto";
+                }
+            }
+            return s;
+        }
+        public void Eliminar(int codigoProducto)
+        {
+            for (int c = 0; c < 15; c++)
+            {
+                if (codigoProducto == codigo[c])
+                {
+                    codigo[c] = 0;
+                    nombre[c] = "0";
+                    cantidad[c] = 0;
+                    costo[c] = 0;
+
+                }
+            }
+        }
+
+        public Boolean seEncontro(int codigoProducto)
+        {
+            Boolean a = false;
+            for (int c = 0; c < 15; c++)
+            {
+                if (codigoProducto == codigo[c])
+                {
+                    a = true;
+
+                }
+
+            }
+            return a;
+        }
+        public int buscarEspacio()
+        {
+            int regresar = -1;
+            for (int c = 0; c < 15; c++)
+            {
+                if (costo[c]==0)
+                {
+                    return regresar = c;
+                }
+            }
+            return regresar;
+
+        }
+        //public void insertar(int posicion, int codigos, string nombres, int cantidades, double costos)
+        //{
+        //    codigo[posicion] = codigos;
+        //    nombre[posicion] = nombres;
+        //    cantidad[posicion] = cantidades;
+        //    costo[posicion] = costos;
+        //}
+
+
+
+
+    }
+}
+
